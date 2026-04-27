6f2033c9708950751a08355b21769538b07d0a77
Nelson <bolkotanelson@gmail.com>
2026-04-27 19:28:43 -0300

    atv1 do botao do luis komar

---
+++ js/atividade1.js
@@ -0,0 +1,6 @@
+const titulo = document.getElementById('titulo');
+const botao = document.getElementById('botao');
+
+botao.addEventListener("click", function(){
+    titulo.innerText = "botao do luis antonio"
+})
